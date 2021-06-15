8f2db1077166e0f6c83a5be3e3b3a1c86aaa4187
hst.paulo <hst.paulo@gmail.com>
2021-06-15 20:49:56 -0300

    aplicando middleware redux-thunk

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 // middlewares
 import promise from 'redux-promise';
 import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducers'
@@ -15,7 +16,7 @@ const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 // todo o estado fica armazenado no objeto store
-const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}> 
