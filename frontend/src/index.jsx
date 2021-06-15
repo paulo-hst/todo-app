2acf2a3f1f050a6b5568e4ff3b553a8909db7e15
hst.paulo <hst.paulo@gmail.com>
2021-06-15 19:31:40 -0300

    configurando o redux devtools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,8 +7,11 @@ import { Provider } from 'react-redux'
 import App from './main/app'
 import reducers from './main/reducers'
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 // todo o estado fica armazenado no objeto store
-const store = createStore(reducers)
+const store = createStore(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}> 
