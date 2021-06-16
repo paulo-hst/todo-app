39b4309cbd6236409b5be9b9a721ab42bcf9fad8
hst.paulo <hst.paulo@gmail.com>
2021-06-15 21:26:09 -0300

    melhorando a action de pesquisa e finalizando o projeto

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -52,7 +52,7 @@ class TodoForm extends Component {
                     <IconButton 
                         style='info' 
                         icon='search' 
-                        onClick={() => search()}
+                        onClick={ search }
                     />
                     <IconButton 
                         style='default' 
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -8,7 +8,7 @@ const todoReducer = (state = INITIAL_STATE, action) => {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
         case 'TODO_SEARCHED':
-            return { ...state, list: action.payload.data }
+            return { ...state, list: action.payload }
         case 'TODO_CLEAR':
             return { ...state, description: ''}
         default:
